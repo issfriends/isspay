529d347d17966ce3d47032f2ed62c22a0289af63
vichsu <vic.hsu@silkrode.com.tw>
2021-02-21 21:11:57 +0800

    add: add order struct

--- internal/app/ordering/order_svc.go
+++ internal/app/ordering/order_svc.go
@@ -5,10 +5,29 @@ import (
 
 	"github.com/issfriends/isspay/internal/app/model"
 	"github.com/issfriends/isspay/internal/app/query"
+	"github.com/shopspring/decimal"
 )
 
+type OrderDatabaser interface {
+}
+
 type OrderServicer interface {
-	CreateOrder(ctx context.Context, order *model.Order) error
-	CancelOrderByUID(ctx context.Context, orderUID string) error
+	CreateOrder(ctx context.Context, order *model.Order) (walletBalance decimal.Decimal, err error)
+	CancelOrderByUID(ctx context.Context, orderUID string) (walletBalance decimal.Decimal, err error)
 	ListOrders(ctx context.Context, q *query.ListOrdersQuery) error
+}
+
+func (svc orderingSvc) CreateOrder(ctx context.Context, order *model.Order) (walletBalance decimal.Decimal, err error) {
+	// 鄭言竹交給你了
+	return decimal.Zero, nil
+}
+
+func (svc orderingSvc) CancelOrderByUID(ctx context.Context, orderUID string) (walletBalance decimal.Decimal, err error) {
+	// 鄭言竹交給你了
+	return decimal.Zero, nil
+}
+
+func (svc orderingSvc) ListOrders(ctx context.Context, q *query.ListOrdersQuery) error {
+	// 鄭言竹交給你了
+	return nil
 }
--- internal/app/ordering/svc.go
+++ internal/app/ordering/svc.go
@@ -1,5 +1,17 @@
 package ordering
 
+type Databaser interface {
+	OrderDatabaser
+}
+
+func New() Servicer {
+	return &orderingSvc{}
+}
+
 type Servicer interface {
 	OrderServicer
+}
+
+type orderingSvc struct {
+	db Databaser
 }
